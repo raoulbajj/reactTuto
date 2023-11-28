c883be0fc682779465b5a43d8663475604e736c6
raoulbajj@mac <raoul.bajjani@epitech.eu>
2023-11-28 09:35:34 +0100

    UPDATE: pushfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./CSS/App.css"
-import Test from './CSS/Components/TestComponents/Test.jsx'
-import Test2 from './CSS/Components/TestComponents/Test2.jsx'
+import Test from './Components/TestComponents/Test.jsx'
+import Test2 from './Components/TestComponents/Test2.jsx'
 
 function App() {
 
