dbb2d3a07325798b1480a621718ac96143926727
raoulbajj@mac <raoul.bajjani@epitech.eu>
2023-11-28 10:50:54 +0100

    UPDATE : push amaury

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import "./CSS/App.css"
 import Test from './Components/TestComponents/Test.jsx'
 import Test2 from './Components/TestComponents/Test2.jsx'
+import Test3 from './Components/TestComponents/Test3.jsx'
+import Navbar from './Components/TestComponents/Navbar.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <main className='flex flex-col gap-5'>
-      <Test />
-      <Test2 />
+
     </main>
   )
 }
---
+++ src/Components/TestComponents/Test3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test3 = () => {
+  return (
+    <div className='bg-red-400 p-2 text-center'>
+      <p>Composant test 3</p>
+    </div>
+  )
+}
+
+export default Test3
